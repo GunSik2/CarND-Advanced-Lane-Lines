c67cab997610de4574983a461cf6ac01f7f98fa2
GunSik Choi <cgshome2@gmail.com>
2017-06-05 06:54:48 +0900

    expample added

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 def warper(img, src, dst):
 
     # Compute and apply perpective transform
@@ -6,3 +9,9 @@ def warper(img, src, dst):
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
 
     return warped
+
+def histogram():
+    binary_warped= np.array([[1, 2, 3], [1, 2, 3]])
+    print(binary_warped)
+
+histogram()
